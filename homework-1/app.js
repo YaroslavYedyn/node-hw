f2ab8556ecfe23be262b89bfa8593edb40847989
Grafin425 <clavik.edin@gmail.com>
2021-02-18 08:59:56 +0200

    homework-1(task 2)

--- homework-1/app.js
+++ homework-1/app.js
@@ -1,2 +1,37 @@
+const path = require('path')
+const fs = require('fs')
+const dirPath = path.join(__dirname, 'time')
+console.log(dirPath);
 
+function readDir(pathName) {
+    fs.readdir(pathName, (err, files) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        files.forEach(fileName => {
+            const pathFile = path.join(pathName, fileName)
 
+            fs.stat(pathFile, (err1, stats) => {
+                if (err1) {
+                    console.log(err1);
+                    return;
+                }
+                if (stats.isDirectory() === true) {
+                    return readDir(pathFile);
+                } else {
+                    const newPath = path.join(__dirname, 'time',fileName)
+                    fs.rename(pathFile, newPath, err2 => {
+                        if (err2) {
+                            console.log(err2);
+                            return;
+                        }
+                    })
+                }
+
+            })
+        })
+    })
+}
+
+readDir(dirPath)
