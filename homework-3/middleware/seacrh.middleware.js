3ca6850720eeba6941eb7a38a94a7fbe7977d155
Grafin425 <clavik.edin@gmail.com>
2021-02-23 23:07:37 +0200

    search middleware

--- homework-3/middleware/seacrh.middleware.js
+++ homework-3/middleware/seacrh.middleware.js
@@ -1,10 +1,19 @@
 const errorMessages = require('../constant/error.messages')
+const errorCodes = require('../constant/errorCodes.enum')
 
 
 module.exports = {
     checkIsValid: (req, res, next) => {
-        if (!req.query) {
-            throw new Error(errorMessages.NOT_VALID["default"])
+        try {
+            if (!req.query) {
+                throw new Error(errorMessages.NOT_VALID["default"])
+            }
+            if (req.query.name === '') {
+                throw new Error(errorMessages.EMPTY["default"])
+            }
+        } catch (e) {
+            res.status(errorCodes.BAD_REQUEST).json(e.message);
         }
+
     }
 }
