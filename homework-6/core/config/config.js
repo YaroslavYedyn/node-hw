8c5c0a18a3fdd26a2fd3a97db2d2712ddc9fe9f4
Grafin425 <clavik.edin@gmail.com>
2021-03-05 19:34:03 +0200

    fix env

--- homework-6/core/config/config.js
+++ homework-6/core/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: 5050,
-    URL_MONGO_DATABASE: 'mongodb://localhost/hw-5',
+    PORT: process.env.PORT || 5050,
+    URL_MONGO_DATABASE: process.env.URL_MONGO_DATABASE || 'mongodb://localhost/hw-5',
 };
