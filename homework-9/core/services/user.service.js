9928c5005b62fba573f714dc1ce27b6f01d40749
paltashka <paltashka50@gmail.com>
2021-03-15 18:08:17 +0200

    homework-9

--- homework-9/core/services/user.service.js
+++ homework-9/core/services/user.service.js
@@ -22,6 +22,22 @@ module.exports = {
     },
     getSingleUser: (query) => User.findOne(query),
     createUser: (object) => User.create(object),
-    updateUser: (params, updateBody) => User.updateOne(params, updateBody),
+    updateUser: async (query, updateBody) => {
+        const {
+            limit, keys, filters, sort, skip, page
+        } = baseQueryBuilder(query);
+        const objectFilter = userObjectFilter(filters, keys);
+
+        const user = await User.updateOne(objectFilter, updateBody).limit(+limit).skip(skip).sort(sort);
+        const count = await User.countDocuments(objectFilter);
+
+        return {
+            data: user,
+            page,
+            limit,
+            count,
+            pages: Math.ceil(limit / count)
+        };
+    },
     removeUser: (id) => User.deleteOne({ _id: id }),
 };
